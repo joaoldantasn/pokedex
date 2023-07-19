e09d8753fb94c8799a87d58d3addbcd4d12e7579
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-07-19 09:22:29 -0300

    poke-search e poke-list criados

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,9 +4,13 @@ import { CommonModule } from '@angular/common';
 //Module Routing
 import { RoutingModule } from './routing.module';
 
+//Module
+import { SharedModule } from '../shared/shared.module';
+
 //Pages
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+
 
 
 @NgModule({
@@ -16,7 +20,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
