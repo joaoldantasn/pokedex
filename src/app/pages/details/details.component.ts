885d4e375da619ad2fe8dc5774abbae1bf76874b
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-07-19 20:18:59 -0300

    Adiçao de Error e Loading

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -15,6 +15,8 @@ export class DetailsComponent implements OnInit {
   private urlName:string = 'https://pokeapi.co/api/v2/pokemon-species';
 
   public pokemon: any;
+  public isLoading:boolean = false;
+  public apiError:boolean = false;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -32,6 +34,10 @@ export class DetailsComponent implements OnInit {
     return forkJoin([pokemon, name]).subscribe(
       res => {
         this.pokemon = res;
+        this.isLoading = true;
+      },
+      error => {
+        this.apiError = true;
       }
     );
   }
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -10,6 +10,7 @@ export class PokeListComponent implements OnInit {
 
   private setAllPokemons:any;
   public getAllPokemons:any;
+  public apiError:boolean = false;
 
 
   constructor(private pokeApiService: PokeApiService){}
@@ -18,6 +19,9 @@ export class PokeListComponent implements OnInit {
       res => {
         this.setAllPokemons = res.results;
         this.getAllPokemons = this.setAllPokemons;
+      },
+      error => {
+        this.apiError = true;
       }
     );
   }
